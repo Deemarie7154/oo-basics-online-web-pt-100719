da28647033c6c11afe7e99521c24376adf8772ec
Dwight Marie <dwightmarie@gmail.com>
2019-10-27 17:06:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -25,6 +25,19 @@ class Shoe
    end
   
   
+    def material=(material) 
+     @material = material
+   end
+   
+   def material
+     @material
+   end
   
-  
+    def condition=(condition) 
+     @condition = condition
+   end
+   
+   def condition
+     @condition
+   end
 end
